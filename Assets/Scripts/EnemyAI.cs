c4ec3443c6887ac9a12f22a8744988bfda808e09
Wayne33 <wuwei1103@gmail.com>
2023-07-19 18:28:51 +0800

    1. Animator Controller: - Learn about animation state machines and create simple animation; - Animation Transiton Conditons; - Trigger Animation in code; - Use Animation Events; 2. Create Player Health 3.Debug: 修复在攻击状态下，敌人不转向的问题

--- Assets/Scripts/EnemyAI.cs
+++ Assets/Scripts/EnemyAI.cs
@@ -7,7 +7,7 @@ public class EnemyAI : MonoBehaviour
 {
     [SerializeField] Transform target;
     [SerializeField] float chaseRange = 5f;  //how far away from the enemy before the enemy starts chasing the player
-
+    [SerializeField] float turnSpeed = 5f;
 
     NavMeshAgent navMeshAgent;
     float distanceToTarget = Mathf.Infinity;
@@ -39,6 +39,7 @@ public class EnemyAI : MonoBehaviour
 
     private void EngageTarget()
     {
+        FaceTarget();
         if (distanceToTarget >= navMeshAgent.stoppingDistance)
         {
             ChaseTarget();
@@ -52,12 +53,22 @@ public class EnemyAI : MonoBehaviour
 
     private void ChaseTarget()
     {
+
+        GetComponent<Animator>().SetBool("attack", false);
+        GetComponent<Animator>().SetTrigger("move");
         navMeshAgent.SetDestination(target.position);
     }
 
     private void AttackTarget()
     {
-        Debug.Log(name + " has seeked and is destroying " + target.name);
+        GetComponent<Animator>().SetBool("attack", true);
+    }
+
+    private void FaceTarget()
+    {
+        Vector3 direction = (target.position - transform.position).normalized;
+        Quaternion lookRotation = Quaternion.LookRotation(new Vector3(direction.x, 0, direction.z));
+        transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * turnSpeed);
     }
 
 
